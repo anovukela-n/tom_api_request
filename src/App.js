20c41acedb52231cbf987301c4f43090c8c279bc
unknown <n.anovukela@gmail.com>
2023-04-28 21:24:28 +0200

    Merlynn assessment react API intergration assessment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function DrinkChoiceForm() {
+  const [metadata, setMetadata] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch("https://api.up2tom.com/v3/models", {
+      method: "GET",
+      headers: {
+        
+        "Authorization":"Token  9307bfd5fa011428ff198bb37547f979",
+        "Content-Type": "application/vnd.api+json"
+      }});
+
+      const data = await response.json();
+    //  console.log(data);
+      setMetadata(data);
+    };
+
+    fetchData();
+  }, []);
+
+  if (!metadata) {
+    return <div>Loading...</div>;
+  }
+  //else {
+   /*  return(
+    <div>Got data</div>); } */
+
+
+ // const { modelName, inputVariables } = metadata;
+ // console.log(modelName);
+  console.log(metadata.data);
+
+
+
+   return (
+    <div>
+      <h2>Drinks choice</h2>
+      <form>
+        {metadata.data.map((variable) => (
+          <div key={variable.name}>
+            <label htmlFor={variable.attributes.name}>{variable.label}</label>
+            <input type={variable.type} id={variable.name} name={variable.name} />
+          </div>
+        ))}
+        <button type="submit">Submit</button>
+      </form>
     </div>
-  );
+  ); 
 }
 
-export default App;
+export default DrinkChoiceForm;
